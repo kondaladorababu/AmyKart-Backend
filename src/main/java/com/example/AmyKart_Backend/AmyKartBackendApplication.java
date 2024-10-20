187b9e4099dd3bcb65115051b3e3cf141d82467a
dkondala-clgx <125888882+dkondala-clgx@users.noreply.github.com>
2024-10-20 21:30:58 +0530

    Intital Setup

---
+++ src/main/java/com/example/AmyKart_Backend/AmyKartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.AmyKart_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmyKartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmyKartBackendApplication.class, args);
+	}
+
+}
