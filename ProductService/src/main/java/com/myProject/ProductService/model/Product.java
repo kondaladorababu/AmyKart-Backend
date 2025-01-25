3e6de2233913b780c1c021f3364ad9db191e22fd
kondaladorababu <kondaladorababu@gmail.com>
2025-01-25 13:37:55 +0530

    Product Service Basic CRUD Implemented Succesfully

---
+++ ProductService/src/main/java/com/myProject/ProductService/model/Product.java
@@ -0,0 +1,88 @@
+package com.myProject.ProductService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String title;
+    double price;
+    String description;
+    String image;
+    String category;
+    int rating;
+
+    public Product() {
+    }
+
+    public Product(String title, double price, String description, String image, int rating, String category) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+        this.rating = rating;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
